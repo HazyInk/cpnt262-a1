8336dd8b8b21d844140a7a3609091da74eeb23b4
HazyInk <reillysheapaston@gmail.com>
2023-10-30 21:13:58 -0600

    added a selectable list to the form using JS

--- client.js
+++ client.js
@@ -28,3 +28,21 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
   navMenu.classList.remove("active");
 //   Need to add Toggle aria-expanded value here as well because it stays as true when you click a menu item
 }))
+
+$(document).ready(function(){
+
+  //hides dropdown content
+  $(".size_chart").hide();
+  
+  //unhides first option content
+  $("#option1").show();
+  
+  //listen to dropdown for change
+  $("#size_select").change(function(){
+    //rehide content on change
+    $('.size_chart').hide();
+    //unhides current item
+    $('#'+$(this).val()).show();
+  });
+  
+});
